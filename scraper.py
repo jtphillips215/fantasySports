8e0b2a94ee3191232e8f7f727b4e421447787973
jtphillips215 <jtphillips215@gmail.com>
2023-02-16 12:39:49 -0600

    base url and concatonated url providing information steam when tested with print statement

--- scraper.py
+++ scraper.py
@@ -2,7 +2,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.google.com/'
+# base URL as site uses RESTful scheme
+BASE_URL = 'http://racing-reference.info/driver-loop-data-stats/'
+
+# sending html request to get site data
+# were just ugly concatonating the url here as a test, will be set programatically later
+url = BASE_URL + 'truexma02/W/2022/'
 page = requests.get(url)
 
-print(page)
+# creating our soup to parse
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# testing data flow is successful
+print(soup)
