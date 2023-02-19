dfb2afbd5c73939687c9a7b0a3665f52bcc8ebbf
jtphillips215 <jtphillips215@gmail.com>
2023-02-19 00:58:54 -0600

    updated code with comments, scraper returning data to main

--- scraper.py
+++ scraper.py
@@ -10,5 +10,6 @@ def scrape(url):
     # creating soup to parse
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    data_frames = pd.read_html(page.text)
-    return data_frames
+    # using pandas to read html and create data frame to return that data to main
+    data_frame = pd.read_html(page.text)
+    return data_frame
